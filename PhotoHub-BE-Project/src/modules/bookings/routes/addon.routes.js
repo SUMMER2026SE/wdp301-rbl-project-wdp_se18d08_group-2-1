2209d0bd4979fbb4e9f025f382d5b6fd6374c87c
chip1810 <lechitrung1810@gmail.com>
2026-07-23 19:35:26 +0700

    fix deploy

--- PhotoHub-BE-Project/src/modules/bookings/routes/addon.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AddonController = require("../controllers/AddonController");
-const authenticate = require("../../../middlewares/authenticate");
-
-router.get("/:bookingId", authenticate, (req, res) => AddonController.getAddonByBookingId(req, res));
-router.post("/:bookingId", authenticate, (req, res) => AddonController.createOrUpdateAddon(req, res));
-
-module.exports = router;
